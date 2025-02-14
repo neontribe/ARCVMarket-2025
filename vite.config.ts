13df56398e2b4f9fc10e9d5ab44bfb68d15d5ceb
charles strange <charles@neontribe.co.uk>
2025-02-14 09:28:41 +0000

    feature: fix pwa

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import VueDevTools from 'vite-plugin-vue-devtools';
 import { VitePWA } from 'vite-plugin-pwa';
 import { execSync } from 'child_process';
 import legacy from '@vitejs/plugin-legacy';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -38,7 +39,33 @@ export default defineConfig({
                 // nudge to portrait for phones and landscape for devices
                 orientation: 'natural',
                 theme_color: '#a74e94',
-                background_color: '#fff'
+                background_color: '#fff',
+                icons: [
+                    {
+                        "src": "/icons/pwa-192x192.png",
+                        "sizes": "192x192",
+                        "type": "image/png",
+                        "purpose": "any"
+                    },
+                    {
+                        "src": "/icons/pwa-512x512.png",
+                        "sizes": "512x512",
+                        "type": "image/png",
+                        "purpose": "any"
+                    },
+                    {
+                        "src": "/icons/pwa-maskable-192x192.png",
+                        "sizes": "192x192",
+                        "type": "image/png",
+                        "purpose": "maskable"
+                    },
+                    {
+                        "src": "/icons/pwa-maskable-512x512.png",
+                        "sizes": "512x512",
+                        "type": "image/png",
+                        "purpose": "maskable"
+                    }
+                ]
             }
         })
     ],
